cfeccd63dce000a52b14d9ec7293148b2cb14d46
ProgramistaiJa <programista@i-ja.pl>
2019-04-22 08:43:36 +0200

    Build a Quiz Component with React

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
         }
     }
     render(){
-        const isQuizEnd = false
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.lenght)
         return(
             <div>
                 {isQuizEnd ? <QuizEnd /> :
